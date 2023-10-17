325c40d6a709c56dd9984ec6b6667d55c726a9c9
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2023-10-17 19:44:04 +0100

    js

--- flor_blanca/static/scripts/formControl.js
+++ flor_blanca/static/scripts/formControl.js
@@ -135,3 +135,8 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+(() => {
+  window.onpageshow = (event) => {
+    if (event.persisted) window.location.reload();
+  };
+})();
